35d8e69188be1697556144fb59fb5eb58773810f
ofa-abdullayeva <abdullayevaofa25@gmail.com>
2021-07-17 09:57:12 +0400

    Array foreach()

---
+++ task-2/script.js
@@ -0,0 +1,25 @@
+// 2. Ededlerin cemini ve hasilini qaytaran funksiya yazin
+
+let arr = [5,2,7,4,8,12];
+
+let cem = 0
+
+function sum(){
+    for(i=0;i<arr.length;i++){
+        console.log(cem = cem + arr[i])
+    }
+}
+sum();
+console.log(cem)
+
+// let arr1 = [1,3,5];
+
+// let sum = 0 
+// function total(){
+//     for(i=0;i<arr1.length;i++){
+//         return sum *= arr1[i] 
+        
+//     }
+// }
+// console.log(total());
+// console.log(sum);
--- task-3/script.js
+++ task-3/script.js
@@ -1,21 +1,30 @@
 
 //Task-3
 
-//  var person = [ 
-//     {
-//         name: 'Kazim',
-//         role: 'react dev',
-//         status: true
-//     },
-//     {
-//         name: 'Vugar',
-//         role: 'angular dev',
-//         status: false
-//     },
-//     {
-//      name: 'Mugan',
-//         role: 'javascript dev',
-//         status: true
-//     }];
+ var person = [ 
+    {
+        name: 'Kazim',
+        role: 'react dev',
+        status: true
+    },
+    {
+        name: 'Vugar',
+        role: 'angular dev',
+        status: false
+    },
+    {
+     name: 'Mugan',
+        role: 'javascript dev',
+        status: true
+    }];
 //Statusu false olanlarin promotion almadigini, true olanlarin ise promotion aldigini eks etdiren cumle yazin
 //ipucu - for, if
+var x = person.forEach((item)=>{
+    if(item.status==true){
+          console.log('Receives promotion')
+     }
+     else{
+         console.log('promotion does not rise')
+     }
+})
+
