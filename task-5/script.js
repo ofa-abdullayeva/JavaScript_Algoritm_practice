899ac5f14bb23390bb96bf94524e53f9b624da41
ofa-abdullayeva <abdullayevaofa25@gmail.com>
2021-07-19 19:14:21 +0400

    Task-5

--- task-5/script.js
+++ task-5/script.js
@@ -2,15 +2,31 @@
 //Task-5
 
 //5. ele bir funksiya yazin ki object oturulende true, array oturulende false versin(funksiyani cagirarken arqument olaraq otureceyiniz object true verecek, array ise false)
-control(25)
-function control(a) {
-    if (typeof a == Object) {
-        console.log('true');
-    }
-    else if (typeof a == Array) {
+// var b = [];
+// var c = {};
+
+
+// const checkelem = (a) => {
+//     if (typeof (a) === typeof ({})) {
+//         console.log('True');
+//     }
+//     else if(typeof (a) === typeof Array.isArray) {
+//         console.log('False');
+//     }
+// };
+
+// checkelem([]);
+
+
+const checkelem = (a) => {
+    if (Array.isArray (a)){
         console.log('false');
     }
-
+    else if (typeof a === 'object'){
+        console.log('True');
+    }
 }
-// console.log(control({age:25}));
-console.log(control(typeof[25, 35, 45, 55]));
+
+// let a = {name:'Ofeliya',surname:'Abdullayeva'};
+let a = [5,52,75,85]
+checkelem(a)
